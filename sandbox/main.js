dec9693ca59862dcfbf2f921ee70e34cdfab93dc
Duke4Coco <u531156798@byui.edu>
2023-01-11 07:03:36 +0200

    Modified my sandbox files.

--- sandbox/main.js
+++ sandbox/main.js
@@ -4,3 +4,9 @@ let area = radius **2 * PI;
 
 console.log(area)
 
+
+let avgOfThreeNums = function name(num1, num2, num3) {
+   let avg = num1 + num2 + num3 / 3;
+    return avg
+}
+console.log(avgOfThreeNums(2, 3, 17));
