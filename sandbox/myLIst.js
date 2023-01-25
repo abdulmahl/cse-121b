883c6aba027a322b06c1e1f219af929c4d264372
Duke4Coco <u531156798@byui.edu>
2023-01-25 21:37:54 +0200

    Different options of iterating through a list.

--- sandbox/myLIst.js
+++ sandbox/myLIst.js
@@ -2,6 +2,16 @@ let myList = document.getElementById("myList");
 
 let friends = ["Jane", "Gary", "Nomsa", "Larry"]
 
+for (let i = 0; i < friends.length; i ++ ) {
+    let listItem = document.createElement("li");
+    listItem.textContent = friends[i];
+    myList.append(listItem);
+}
+
+for (friend of friends) {
+    myList.innerHTML += "<li>" + friend + "</li>";
+}
+
 friends.forEach(friend => {
     let listItem = document.createElement("li");
     listItem.textContent = friend;
