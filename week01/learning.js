d26f11a844177f71f6d4bbfa8339bfdfce8c81dd
abdulmahl <mahalanguma87@gmail.com>
2023-10-09 11:07:46 +0200

    .

--- week01/learning.js
+++ week01/learning.js
@@ -12,7 +12,7 @@ console.log("The total is "+theTotal)
 function total(...theNumbers){
 	let sum = 0
 	for(let aNumber in theNumbers){
-		sum += aNumber*1//Want to know why aNumber is multiplied by 1? Remove it and find out. :)
+		sum += aNumber*1 //Want to know why aNumber is multiplied by 1? Remove it and find out. :)
 	}
 	return sum
 }
